e137c546f14baa8b87e84be9cfb35182ce2ccaa3
stefanocaramagno <stefano.caramagno@gmail.com>
2025-12-01 23:22:06 +0100

    made microservices bootable

---
+++ data-collector-service/src/main/java/flights/datacollector/DataCollectorServiceApplication.java
@@ -0,0 +1,12 @@
+package flights.datacollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataCollectorServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataCollectorServiceApplication.class, args);
+    }
+}
---
+++ user-manager-service/src/main/java/flights/usermanager/UserManagerServiceApplication.java
@@ -0,0 +1,12 @@
+package flights.usermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserManagerServiceApplication.class, args);
+    }
+}
