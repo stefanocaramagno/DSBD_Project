58192cc2aa301ffcd1b399828ff086c365577bc1
stefanocaramagno <stefano.caramagno@gmail.com>
2025-12-01 23:58:19 +0100

    fixed some errors

--- user-manager-service/src/main/java/flights/usermanager/api/UserController.java
+++ user-manager-service/src/main/java/flights/usermanager/api/UserController.java
@@ -4,7 +4,6 @@ import flights.usermanager.api.dto.UserRegistrationRequest;
 import flights.usermanager.api.dto.UserResponse;
 import flights.usermanager.service.UserService;
 import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
